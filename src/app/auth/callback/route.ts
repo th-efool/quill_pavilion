8e85483fa1910634b2f8e9b607b6664ec80bb392
Agrim Singh <asas05202415@gmail.com>
2026-03-26 11:06:48 +0530

    Delete route.ts

--- src/app/auth/callback/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET(req: Request) {
-    const origin = process.env.NEXT_PUBLIC_SITE_URL!
-
-    return NextResponse.redirect(`${origin}/dashboard/projects`)
-}
